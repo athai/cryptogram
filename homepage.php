f034f28538de6bd99ad72d4451c9a1ff6d7445a3
Aileen Thai <dapper.whale@gmail.com>
2017-06-01 21:14:18 -0700

    Reformat code

--- homepage.php
+++ homepage.php
@@ -9,40 +9,36 @@
 <?php include 'header.html';?>
 
 <!-- recent photos: gallery -->
-      <!-- move this href tag later -->
-        <?php
-          include 'connectvarsEECS.php';
-        
-          $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-          if (!$conn){
-            die('Could not connect: ' . mysql_error());
-          }
-          $query = "SELECT pictureURL FROM CryptidPhotos";
-          $result = mysqli_query($conn, $query);
-          if (!$result){
-            die("Query failed");
-          }
-          $num_fields = mysqli_num_fields($result);
-        
-          for ($i=0; $i<$num_fields; $i++){
-            $link = mysqli_fetch_field($result)
-            echo "<div class='row'>"
-            echo "<div class='col-md-6'>"
-            echo "<div class='thumbnail'>"
-            echo "<a href='#'>"
-            echo "<img src='$link'";
-            //echo "<div class='description'>";
-            //echo "<p>$</>";
-            //echo "</div>";
-            echo "</a>";
-          }
-        
-          mysqli_free_result($result);
-          mysqli_close($conn);
-        ?>
-    </div>
-  </div>
-</div>
+<?php
+  include 'connectvarsEECS.php';
+
+  $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+  if (!$conn){
+    die('Could not connect: ' . mysql_error());
+  }
+  $query = "SELECT pictureURL FROM CryptidPhotos";
+  $result = mysqli_query($conn, $query);
+  if (!$result){
+    die("Query failed");
+  }
+  $num_fields = mysqli_num_fields($result);
+
+  for ($i=0; $i<$num_fields; $i++){
+    $link = mysqli_fetch_field($result);
+    echo "<div class='row'>
+            <div class='col-md-6'>
+              <div class='thumbnail'>
+                <a href='#'>
+                  <img src='$link'>
+                </a>
+              </div>
+            </div>
+          </div>";
+  }
+
+  mysqli_free_result($result);
+  mysqli_close($conn);
+?>
 
 </body>
 </html>
