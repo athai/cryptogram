04ead312ae10bc869cc19b74a5b2357c608849c9
Aileen <dapper.whale@gmail.com>
2017-06-01 18:33:07 -0700

    Testing

---
+++ homeview.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!-- homeview.php -->
+<html>
+  <head>
+    <title>Cryptogram</title>
+  </head>
+<body>
+
+<?php
+  include 'header.html';
+  include 'homepage.html';
+?>
+
+</body>
+</html>
